ccf62e1f885566f51402c7ebbc7781d2f141c915
Carl A Dunham <cdunham@gmail.com>
2013-11-30 00:30:36 -0500

    should have final paddle hit detection working, fixed score text

--- games/bounce/Ball.py
+++ games/bounce/Ball.py
@@ -1,20 +1,22 @@
-from tkinter import *
+
 import random
-import time
 
 class Ball:
         def __init__(self, canvas, color):
                 self.canvas = canvas
                 self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
-                self.canvas.move(self.id, 245, 100)
-                starts = [-3, -2, -1, 1, 2, 3]
+                self.canvas.move(self.id, 0, 0)
+                starts = [2, 3, 4]
                 random.shuffle(starts)
                 self.x = starts[0]
-                self.y = starts[1]
+                random.shuffle(starts)
+                self.y = starts[0]
                 self.score = 0
-                self.canvas.create_text(10, 200, text=self.score)
+                self.score_id = self.canvas.create_text(10, 200, text=self.score)
                 self.canvas_height = self.canvas.winfo_height()
                 self.canvas_width = self.canvas.winfo_width()
+
+                #print("self.x=%d, self.y=%d" % (self.x, self.y))
 
         def coords(self):
             return self.canvas.coords(self.id)
@@ -22,19 +24,26 @@ class Ball:
         def check(self, paddle):
             paddle_pos = paddle.coords()
             ball_pos = self.coords()
-            
-            if (((ball_pos[2] >= paddle_pos[0]) and (ball_pos[0] <= paddle_pos[2])) and                    
-                (((ball_pos[3] >= paddle_pos[1]) and (ball_pos[3] <= paddle_pos[3])) or
-                 ((ball_pos[1] >= paddle_pos[1]) and (ball_pos[1] <= paddle_pos[3])))):
-                        self.y = -self.y
-                        self.score = self.score + 1
-                        print(self.score)
-                        
-            elif (((ball_pos[3] >= paddle_pos[1]) and (ball_pos[1] <= paddle_pos[3])) and                    
-                (((ball_pos[2] >= paddle_pos[0]) and (ball_pos[2] <= paddle_pos[2])) or
-                 ((ball_pos[0] >= paddle_pos[0]) and (ball_pos[0] <= paddle_pos[2])))):
-                        self.x = -self.x
-                        self.score = self.score + 1
+
+            if self.id in self.canvas.find_overlapping(*paddle_pos):
+
+                x = min(abs(paddle_pos[0] - ball_pos[2]), abs(paddle_pos[2] - ball_pos[0]))
+                y = min(abs(paddle_pos[1] - ball_pos[3]), abs(paddle_pos[3] - ball_pos[1]))
+
+                #print("self.x=%d, self.y=%d, x=%d, y=%d" % (self.x, self.y, x, y))
+
+                if x < abs(self.x):
+                    self.x = -self.x
+                elif y < abs(self.y):
+                    self.y = -self.y
+                else:
+                    print("oops!!")
+                    self.x = -self.x
+                    self.y = -self.y
+                    
+                self.score = self.score + 1
+                self.canvas.itemconfig(self.score_id, text=self.score)
+                #print(self.score)
                                         
 
         def draw(self):
--- games/bounce/bounce game.py
+++ games/bounce/bounce game.py
@@ -1,27 +1,27 @@
+
 from tkinter import *
-import random
+
 import time
+
 from Ball import Ball
 from Paddle import Paddle
 
 tk = Tk()
 tk.title("Game")
 tk.resizable(0, 0)
-tk.wm_attributes("-topmost", 1)
+#tk.wm_attributes("-topmost", 1)
 canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
 
-
-
-
 paddle = Paddle(canvas, 'green')
 ball = Ball(canvas, 'blue')
+
 while 1:
     paddle.draw()
     ball.draw()
     ball.check(paddle)
     tk.update_idletasks()
     tk.update()
-    time.sleep(0.001)
+    time.sleep(0.01)
  
