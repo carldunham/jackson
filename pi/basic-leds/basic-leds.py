dc2af60b7ef74f30a08e45eecc0f23eb0934e0c6
Carl A Dunham <cdunham@gmail.com>
2014-12-27 14:12:59 -0500

    working counter

--- pi/basic-leds/basic-leds.py
+++ pi/basic-leds/basic-leds.py
@@ -1,5 +1,6 @@
 #!/bin/env python
 
+import logging
 import RPi.GPIO as GPIO
 import time
 
@@ -12,12 +13,32 @@ LED_R = 12
 LED_G = 16
 LED_B = 21
 
+INTERVAL = 1   # in seconds
+
 GPIO.setup(LED_R, GPIO.OUT)
 GPIO.setup(LED_G, GPIO.OUT)
 GPIO.setup(LED_B, GPIO.OUT)
 
-GPIO.output(LED_G, ON)
-time.sleep(10)
-GPIO.output(LED_G, OFF)
+logging.info("Starting...")
+
+while True:
+
+    try:
+        for i in xrange(0, 8):
+            logging.info("  %d", i)
+
+            GPIO.output(LED_R, ON if i & 0b100 else OFF)
+            GPIO.output(LED_G, ON if i & 0b010 else OFF)
+            GPIO.output(LED_B, ON if i & 0b001 else OFF)
+
+            time.sleep(INTERVAL)
+    except KeyboardInterrupt:
+        break
+
+    except:
+        logging.exception()
+        break
+
+logging.info("done.")
 
 GPIO.cleanup()
