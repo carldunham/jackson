45d14d02539313ffe5dba06c325e4d8807fc59a2
Carl A Dunham <cdunham@gmail.com>
2014-12-27 23:07:19 -0500

    better rand1

--- pi/leds/plugins/rand1.py
+++ pi/leds/plugins/rand1.py
@@ -19,6 +19,7 @@ def step(interval):
     n = random.choice(control.ALL_LEDS)
 
     control.alloff()
-    control.on(n)
+    time.sleep(0.1)  # so we can see repeats
 
+    control.on(n)
     time.sleep(interval)
