ab59a556c4252e2f761495ce1516d115f175810a
Carl A Dunham <cdunham@gmail.com>
2014-12-27 20:57:12 -0500

    split out functions

--- pi/basic-leds/basic-leds.py
+++ pi/basic-leds/basic-leds.py
@@ -5,8 +5,6 @@ import logging
 import RPi.GPIO as GPIO
 import time
 
-DEBUG = 0
-
 ON = GPIO.HIGH
 OFF = GPIO.LOW
 
@@ -14,57 +12,72 @@ LED_R = 12
 LED_G = 16
 LED_B = 21
 
-INTERVAL = 1   # in seconds
+DEFAULT_INTERVAL = 1   # in seconds
 
 logger = logging.getLogger(__name__)
 
-def main():
-    parser = argparse.ArgumentParser(description='Control red, green and blue LEDs')
-    parser.add_argument('-d', '--debug', type=str, default='INFO', help='set debug level to DEBUG (default: %(default)s)')
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('-q', '--quiet', action='store_true')
-    group.add_argument('-v', '--verbose', action='store_true')
 
-    opts = parser.parse_args()
-
-    global DEBUG
-    DEBUG = opts.debug
-
-    logging.basicConfig(level='INFO')
-    logger.debug('opts="%s"',  opts)
-
+def setup():
     GPIO.setmode(GPIO.BCM)
+
+    alloff()
 
     GPIO.setup(LED_R, GPIO.OUT)
     GPIO.setup(LED_G, GPIO.OUT)
     GPIO.setup(LED_B, GPIO.OUT)
-    
-    logging.info("Starting...")
-    
-    while True:
-    
-        try:
-            for i in xrange(0, 8):
-                logging.info("  %d", i)
-    
-                GPIO.output(LED_R, ON if i & 0b100 else OFF)
-                GPIO.output(LED_G, ON if i & 0b010 else OFF)
-                GPIO.output(LED_B, ON if i & 0b001 else OFF)
-    
-                time.sleep(INTERVAL)
-        except KeyboardInterrupt:
-            break
-    
-        except:
-            logging.exception()
-            break
-    
-    logging.info("done.")
-    
+
+
+def teardown():
+    alloff()
+    GPIO.cleanup()
+
+
+def alloff():
     GPIO.output(LED_R, OFF)
     GPIO.output(LED_G, OFF)
     GPIO.output(LED_B, OFF)
-    GPIO.cleanup()
+
+
+def counter(interval=DEFAULT_INTERVAL):
+
+    for i in xrange(8):
+        logging.info("  %d", i)
+
+        GPIO.output(LED_R, ON if i & 0b100 else OFF)
+        GPIO.output(LED_G, ON if i & 0b010 else OFF)
+        GPIO.output(LED_B, ON if i & 0b001 else OFF)
+
+        time.sleep(interval)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Control red, green and blue LEDs')
+    parser.add_argument('-d', '--debug', type=str, default='INFO', help='set debug level to DEBUG (default: %(default)s)')
+
+    opts = parser.parse_args()
+
+    logging.basicConfig(level=opts.debug)
+    logger.debug('opts="%s"',  opts)
+
+    setup()
+
+    logging.info("Starting...")
+
+    while True:
+
+        try:
+            counter()
+
+        except KeyboardInterrupt:
+            break
+
+        except:
+            logging.exception()
+
+    logging.info("done.")
+
+    teardown()
+
 
 if __name__ == '__main__':
     main()
